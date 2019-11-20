a67f029cb67508ca9e47f95a99335faf12273b15
Hulto <jack.m.mckenna@gmail.com>
2019-11-20 18:37:46 -0500

    $(cat /tmp/comments.txt)

--- app/webserver/functiondb.py
+++ app/webserver/functiondb.py
@@ -1,17 +1,11 @@
-import initdb as db
+#import initdb as db
 import hashlib
 from flask_login import LoginManager
 
 
-
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
-
 from .initdb import User
 
 
-@login_manager.user_loader
 def load_user(user_id):
     # since the user_id is just the primary key of our user table, use it in the query for the user
     return User.query.get(int(user_id))
--- app/webserver/initdb.py
+++ app/webserver/initdb.py
@@ -56,13 +56,6 @@ user_oneeyed = User(fname='Jacob Ruud',
                     passwd=password123_hash,
                     role=0)
 
-
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
-
-
-@login_manager.user_loader
 def load_user(user_id):
     # since the user_id is just the primary key of our user table, use it in the query for the user
     return User.query.get(int(user_id))
