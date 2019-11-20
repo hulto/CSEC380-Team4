26d243d408322505ab659c65e013014d1130b9d9
Hulto <jack.m.mckenna@gmail.com>
2019-11-20 18:43:14 -0500

    $(cat /tmp/comments.txt)

--- app/main.py
+++ app/main.py
@@ -1,20 +1,16 @@
 from flask import render_template, Flask
 from flask_login import login_required, current_user
 
-app = Flask(__name__)
-# app = Blueprint('main', __name__, template_folder='webserver/templates')
+from webserver.auth import auth
+
+app = Flask(__name__, template_folder='webserver/templates')
+app.register_blueprint(auth)
+
 
 @app.route('/')
 def index():
-    return render_template('template.html')
+    return render_template('index.html')
 
-
-# @main.route('/profile')
-# @login_required
-# def profile():
-#     return render_template('profile.html', name=current_user.name)
-
-# app.register_blueprint(auth, url_prefix='/auth')
 
 if __name__ == "__main__":
     app.run(debug=True, port=9000)
