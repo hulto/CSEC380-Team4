c522035a15ec8301abd52428481fc20bd1632b0f
OneEyedGrape <iamruud16@gmail.com>
2019-11-24 23:06:06 -0500

    added testing for activity 3

--- testing/test_act3.py
+++ testing/test_act3.py
@@ -1,5 +1,40 @@
 import pytest
 import requests
 
-def test_hello():
-    assert(1 == 1)
+url = 'http://localhost:9000/login'
+
+
+def test_valid_login():
+    payload = {
+        'username': 'oneeyedgrape',
+        'password': 'password123'
+    }
+
+    with requests.Session() as s:
+        p = s.post(url, data=payload)
+        assert p.status_code == 200
+        assert p.url != url
+
+
+def test_invalid_uname():
+    payload = {
+        'username': 'twoeyedgrape',
+        'password': 'password123'
+    }
+
+    with requests.Session() as s:
+        p = s.post(url, data=payload)
+        assert p.status_code == 200
+        assert p.url == url
+
+
+def test_invalid_password():
+    payload = {
+        'username': 'oneeyedgrape',
+        'password': 'password1234'
+    }
+
+    with requests.Session() as s:
+        p = s.post(url, data=payload)
+        assert p.status_code == 200
+        assert p.url == url
